65be91f3113e9565b881abe4b153b7c453e15743
Lahiru Vimukthi <lvimukthi@peerlessfoods.com.au>
2024-05-20 15:44:14 +0530

    add

--- apps/frontend/src/app/pages/Users.tsx
+++ apps/frontend/src/app/pages/Users.tsx
@@ -13,6 +13,17 @@ const handleDelete = async (id: string) => {
     return response;
 }
 
+const handleEdit = async (id: string) => {
+    var x =  {
+        fullName: "John Doe",
+        email: "assas@gmail.com",
+        phone: "1234567890",
+        password: "12345678"	
+    }
+    const response = await axios.put(`https://backend-test-qll5.onrender.com/user/update/${id}`,x);
+    return response.data;
+}
+
 const Users = () => {
     const { data, error, isLoading, refetch } = useQuery(
         {
@@ -33,8 +44,23 @@ const Users = () => {
         },
     });
 
+    const updateMutation = useMutation({
+        mutationFn: handleEdit,
+        onSuccess: (data) => {
+            console.log('User updated successfully:', data);
+            refetch();
+        },
+        onError: (error) => {
+            console.error('Error updating user:', error);
+        },
+    });
+
     const deleteFunction = async (id: string) => {
         deleteMutation.mutate(id);
+    }
+
+    const updateFunction = async (id: string) => {
+        updateMutation.mutate(id);
     }
 
     if (isLoading) {
@@ -54,22 +80,35 @@ const Users = () => {
         { field: 'fullName', headerName: 'Full Name', width: 200 },
         { field: 'email', headerName: 'Email', width: 200 },
         {
-            field: 'actions',
-            headerName: 'Actions',
+            field: 'delete',
+            headerName: 'Delete',
             width: 150,
-            renderCell: (params: any) => (
+            renderCell: (params: any) => (             
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={() => deleteFunction(params.row._id)}
+                    >
+                        Delete
+                    </Button>                                 
+            ),
+        },        {
+            field: 'edit',
+            headerName: 'Edit',
+            width: 150,
+            renderCell: (params: any) => (             
                 <Button
-                    variant="contained"
-                    color="secondary"
-                    onClick={() => deleteFunction(params.row._id)}
-                >
-                    Delete
-                </Button>
+                variant="contained"
+                color="secondary"
+                onClick={() => updateFunction(params.row._id)}
+            >
+                Edit
+            </Button>                          
             ),
         },
     ];
 
-    const getRowId = (row: any) => row._id; 
+    const getRowId = (row: any) => row._id;
 
     return (
         <div style={{ height: 400, width: '100%' }}>
